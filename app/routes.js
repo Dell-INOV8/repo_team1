4ecdcccae78bd4a651aa78055c7eaeaabb12d348
Jorge Garcia <jorgegarcia525@gmail.com>
2018-10-12 10:17:58 -0500

    implement template

--- app/routes.js
+++ app/routes.js
@@ -29,8 +29,10 @@ module.exports = function(app) {
     // frontend routes =========================================================
     // route to handle all angular requests
 
+
+
     app.get('*', function(req, res) {
-        res.sendfile('public/views/index.html'); // load our public/index.html file
+        res.sendfile('./public/views/index.html'); // load our public/index.html file
     });
 
 };
